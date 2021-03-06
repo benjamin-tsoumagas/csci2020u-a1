ed02eb23f313fb7d26ddefff8beaeb8af92ad702
benjamin-tsoumagas <benjamin.tsoumagas@ontariotechu.net>
2021-03-06 13:08:07 -0500

    Table and TextFields set up

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,53 @@
 package sample;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+
 public class Controller {
+    public TextField precision;
+    public TextField accuracy;
+    @FXML
+    private TableView<SpamDetector> tabView;
+    @FXML
+    private TableColumn<Object, Object> file;
+    @FXML
+    private TableColumn<Object, Object> type;
+    @FXML
+    private TableColumn<Object, Object> prob;
+
+    public static class SpamDetector {
+        private final String fileName;
+        private final String classType;
+        private final double spamProb;
+
+        public SpamDetector(String fileName, String classType, double spamProb) {
+            this.fileName = fileName;
+            this.classType = classType;
+            this.spamProb = spamProb;
+        }
+
+        public String getFileName() { return fileName; }
+        public String getClassType() { return classType; }
+        public double getSpamProb() { return spamProb; }
+    }
+
+    public static class EmailSource {
+        public static ObservableList<SpamDetector> getAllEmails() {
+            ObservableList<SpamDetector> emails = FXCollections.observableArrayList();
+            emails.add(new SpamDetector("abc","def",0.79));
+            return emails;
+        }
+    }
+
+    public void initialize(){
+        file.setCellValueFactory(new PropertyValueFactory<>("fileName"));
+        type.setCellValueFactory(new PropertyValueFactory<>("classType"));
+        prob.setCellValueFactory(new PropertyValueFactory<>("spamProb"));
+        tabView.setItems(EmailSource.getAllEmails());
+    }
 }
